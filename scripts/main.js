7a76641f4f3c80b5944d14e135ebab625efecacb
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-01-29 15:42:07 -0600

    updated

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function()	{
+$('#submit').on('click', onSubmitButtonClick);
+
+function onSubmitButtonClick()	{
+	console.log($('#usernamebox').val());
+	console.log($('#messagebox').val());
+	// var now=timeStamp();
+
+	$.post(
+		'https://nameless-dusk-3865.herokuapp.com/users.json',
+		{
+			user: {
+				message: $('#messagebox').val(),
+				user_name: $('#usernamebox').val(),
+				}
+		}, 
+			function(message){
+				console.log(message);
+				render(message);
+			},
+			'json'
+		);
+	$('#messagebox').val('');
+	$('#usernamebox').val('');
+
+
+
+
+}
+	var getMessages = function() {
+		$.get(
+			'https://nameless-dusk-3865.herokuapp.com/users.json',
+			function(messages) {
+				render(messages);
+			},
+			'json'
+		);
+	};
+
+	var render = function(messages) {
+		var messageRow = _.template('<br><div class="row"><div><strong><%= user_name %></strong></div><div><%= message %></div><div><%= time %></div></div>');
+		console.log(messages);
+		$('#chatbox').html('');
+		for(var i=0; i<messages.length; i++) {
+			if(messages[i].message && messages[i].name && messages[i].time) {
+				$('#chatbox').append(messageRow(messages[i]) );
+			}
+		}
+	};
+
+	setInterval( getMessages, 1000);
+	
+});
+
