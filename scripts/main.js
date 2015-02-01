65478cfc1bc55838083bbad04165526146121489
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-02-01 10:43:52 -0600

    fixed submit button

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function()	{
-$('#submit').on('click', onSubmitButtonClick);
+$('.submit').on('click', onSubmitButtonClick);
 
 function onSubmitButtonClick()	{
 	console.log($('#usernamebox').val());
