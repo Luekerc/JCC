b823de4b3fe1c4c255333a0fbb0e4858beb40fce
Charles Lueker <charleslueker@yahoo.com>
2015-04-05 20:11:34 -0500

    The array of messages finally stops

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,32 @@
 $(document).ready(function()	{
-$('.submit').on('click', onSubmitButtonClick);
+	$('.submit').on('click', onSubmitButtonClick);
 
-function onSubmitButtonClick()	{
-	console.log($('#usernamebox').val());
-	console.log($('#messagebox').val());
+	function onSubmitButtonClick()	{
+		console.log($('#usernamebox').val());
+		console.log($('#messagebox').val());
 
 
-	$.post(
-		'https://nameless-dusk-3865.herokuapp.com/users.json',
-		{
-			user: {
-				message: $('#messagebox').val(),
-				user_name: $('#usernamebox').val(),
-				
-				}
-		}, 
-			function(message){
-				console.log(message);
-				render(message);
-			},
-			'json'
-		);
-	$('#messagebox').val('');
-	$('#usernamebox').val('');
-
-}
-	var getMessages = function() {
-		$.get(
+		$.post(
 			'https://nameless-dusk-3865.herokuapp.com/users.json',
+			{
+				user: {
+					message: $('#messagebox').val(),
+					user_name: $('#usernamebox').val(),
+					
+					}
+			}, 
+				function(message){
+					console.log(message);
+					render(message);
+				},
+				'json'
+			);
+		$('#messagebox').val('');
+		$('#usernamebox').val('');
+
+	}
+	var getMessages = function() {
+		$.get('https://nameless-dusk-3865.herokuapp.com/users.json', 	
 			function(messages) {
 				render(messages);
 			},
@@ -38,6 +37,7 @@ function onSubmitButtonClick()	{
 	var render = function(messages) {
 		var messageRow = _.template('<br><div class="row"><div class="onscreenusername"><%= user_name %>: &nbsp;</div><div class="message"><%= message %></div><div class="timestamp"><%= time_stamp %></div></div><br>');
 		console.log(messages);
+		$('#chatbox').html('');
 			for(var i=0; i<messages.length; i++) {
 			if(messages[i].message && messages[i].user_name && messages[i].time_stamp) {
 				$('#chatbox').prepend(messageRow(messages[i]) );
